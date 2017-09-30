ddcfcd60eb7920373c62e970050f1e59eb13df0f
jkxruby <jkxruby@gmail.com>
2017-09-30 17:12:37 +0800

    add new post function

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,5 @@
 class PostsController < ApplicationController
+before_action :authenticate_user! 
 
 def index
   @group = Group.find(params[:group_id])
@@ -12,16 +13,19 @@ end
 
 def create
    @group = Group.find(params[:group_id])
-   @post = Post.find(params[:id])
+   @post = Post.new(post_params)
    @post.group = @group
    @post.user = current_user
    if @post.save
-     redirect_to group_posts_path(@group)
+     redirect_to group_path(@group)
    else
      render :new
    end
- end 
+ end
 
-
+private
+def post_params
+  params.require(:post).permit(:content)
+end
 
 end
